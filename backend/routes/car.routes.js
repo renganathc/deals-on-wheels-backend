e52fce5023dbfbf35faaecea839b5538f2238932
Renganath Chokkalingam <rengnath1234@gmail.com>
2025-05-22 20:40:48 +0530

    Range Filters Support Added

--- backend/routes/car.routes.js
+++ backend/routes/car.routes.js
@@ -4,13 +4,25 @@ import express from 'express';
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-    const {minPrice, maxPrice, ...remaining} = req.query;
+    const {minPrice, maxPrice, startYear, endYear, startMileage, endMileage, ...remaining} = req.query;
     const query = {...remaining};
     query.price = {};
+    query.year = {};
+    query.km = {};
     minPrice != undefined ? query.price.$gte = Number(minPrice) : null;
     maxPrice != undefined ? query.price.$lte = Number(maxPrice) : null;
+    startYear != undefined ? query.year.$gte = Number(startYear) : null;
+    endYear != undefined ? query.year.$lte = Number(endYear) : null;
+    startMileage != undefined ? query.km.$gte = Number(startMileage) : null;
+    endMileage != undefined ? query.km.$lte = Number(endMileage) : null;
     if (Object.keys(query.price).length === 0) {
         delete query.price;
+    }
+    if (Object.keys(query.year).length === 0) {
+        delete query.year;
+    }
+    if (Object.keys(query.km).length === 0) {
+        delete query.km;
     }
 
     const result = await Car.find(query);
